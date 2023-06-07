663b0127942ba561362003c0f071c891a2b99837
ChuongDE <chuongpntde160221@fpt.edu.vn>
2023-06-07 13:59:26 +0700

    Chuong commit

--- src/java/dao/AccountDAO.java
+++ src/java/dao/AccountDAO.java
@@ -5,6 +5,7 @@
 package dao;
 
 import context.DBConnect;
+import controller.Encode;
 import entity.Account;
 import java.sql.Connection;
 import java.sql.Date;
@@ -12,6 +13,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -185,6 +187,59 @@ public class AccountDAO {
         } catch (Exception e) {
 
         }
+    }
+    
+    public ArrayList checkLogin(String userIDGoogle) {
+        ArrayList<Account> ul = new ArrayList<>();
+        String query = "select * from \"tbAccount\" where \"user_id_google\"= ?";
+            
+        try {
+            conn = new DBConnect().makeConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, userIDGoogle);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Account a = new Account();
+                a.setUserID(rs.getString("userID"));
+                a.setUserName(rs.getString("userName"));
+                a.setPassword(rs.getString("password"));
+                a.setEmail(rs.getString("email"));
+                a.setYearOfBirth(rs.getString("yearOfBirth"));
+                a.setPhone(rs.getString("phone"));
+                a.setImage(rs.getString("image"));
+                a.setRole(rs.getInt("role"));
+                a.setUser_id_google(rs.getString("user_id_google"));
+                ul.add(a);
+            }
+        } catch (Exception e) {
+            System.out.println("Error user: " + e.getMessage());
+        }
+
+        return ul;
+    }
+    
+     //funtion add user with account google
+    public void insertWithIDGoogle(Account a) throws Exception {
+        String query = "INSERT INTO \"tbAccount\"\n"
+                + "           (\"UserName\"\n"
+                + "           ,\"Email\"\n"
+                + "           ,\"user_id_google\",\"Role\", \"Password\")\n"
+                + "           VALUES (?,?,?,?,?);";
+        try {
+            Encode ec = new Encode();
+            String pass = ec.encode("Nolove2@123");
+            conn = new DBConnect().makeConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, a.getEmail());
+            ps.setString(2, a.getEmail());
+            ps.setString(3, a.getUser_id_google());
+            ps.setInt(4, 2);
+            ps.setString(5, pass);
+       
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } 
     }
 
     public static void main(String[] args) {
